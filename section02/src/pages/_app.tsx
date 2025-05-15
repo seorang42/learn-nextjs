d5084b65106e588bd8dbe1b0f4b46bafb479c528
seorang42 <seorang42@naver.com>
2025-05-15 19:14:14 +0900

    2-4 프리페칭

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -10,10 +11,16 @@ export default function App({ Component, pageProps }: AppProps) {
     router.push("/test");
   };
 
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
+
   return (
     <>
       <header>
-        <Link href={"/"}>index</Link>
+        <Link href={"/"} prefetch={false}>
+          index
+        </Link>
         &nbsp;
         <Link href={"/search"}>search</Link>
         &nbsp;
