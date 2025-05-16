7ec6f73f143e7284fe23cd3d8b2b7ebf37d8c252
seorang42 <seorang42@naver.com>
2025-05-16 19:47:13 +0900

    2-12 SSR 2. 실습

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,50 +1,42 @@
 // CSS Module
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode, useEffect } from "react";
-import books from "@/mock/books.json";
+import { ReactNode } from "react";
 import BookItem from "@/components/book-items";
 import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-// SSR 방식으로 사전 렌더링
-export const getServerSideProps = () => {
-  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
-
-  // window.location; 오류 발생 - 서버에선 브라우저에 접근할 수 없음
-  console.log("SSR"); // 서버 콘솔에만 표시
-
-  const data = "hello";
+export const getServerSideProps = async () => {
+  // 병렬로 api 실행
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
 
   return {
     props: {
-      data,
+      allBooks,
+      recoBooks,
     },
   };
 };
 
 export default function Home({
-  data,
+  allBooks,
+  recoBooks,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(data);
-
-  // window.location; 오류 발생 - 서버에서 사전 렌더링 시 window가 undefined
-  useEffect(() => {
-    // useEffect()는 컴포넌트가 마운트 된 이후 실행되기 때문에
-    // window 객체가 존재
-    console.log(window);
-  }, []);
-
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
