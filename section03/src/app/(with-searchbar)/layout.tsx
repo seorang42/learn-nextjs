6312803173f7fb7d0b4442b5ce1eea41ab74a3de
seorang42 <seorang42@naver.com>
2025-05-19 01:42:06 +0900

    3-4 리액스 서버 컴포넌트 이해하기

--- section03/src/app/(with-searchbar)/layout.tsx
+++ section03/src/app/(with-searchbar)/layout.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react";
+import Searchbar from "./searchbar";
 
 export default function Layout({
   children,
 }: Readonly<{ children: ReactNode }>) {
   return (
     <div>
-      <div>임시 서치바</div>
+      <Searchbar />
       {children}
     </div>
   );
