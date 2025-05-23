c003c770261fa8fb635ac0cc190db86ba1e08752
seorang42 <seorang42@naver.com>
2025-05-23 23:05:52 +0900

    7-4 리뷰 재검증 구현하기

--- section07/src/actions/create-review.action.ts
+++ section07/src/actions/create-review.action.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
@@ -19,6 +21,7 @@ export async function createReviewAction(formData: FormData) {
     );
 
     console.log(response);
+    revalidatePath(`/book/${bookId}`);
   } catch (err) {
     console.error(err);
     return;
